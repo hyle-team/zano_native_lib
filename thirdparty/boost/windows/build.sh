e7d1b8691e0655d45eecf2ad04f20e986a07e5f0
Mikhailov Anton <anion155@gmail.com>
2026-04-13 13:56:26 +0400

    feat: openssl windows

--- thirdparty/boost/windows/build.sh
+++ thirdparty/boost/windows/build.sh
@@ -1,36 +1,12 @@
 #!/bin/bash
 
-ROOT=$(realpath $(dirname $0)/../../..)
+SCRIPT_ROOT=$(realpath $(dirname $0))
+ROOT=$(realpath "${SCRIPT_ROOT}/../../..")
 BOOST=$(realpath "${ROOT}/thirdparty/boost/windows")
 cd "$BOOST"
 
 BOOST_VERSION=${BOOST_VERSION:-1.76.0}
 BOOST_TAR_HASH=${BOOST_TAR_HASH:-59cbd8a453c4cbc7e5bc9966101cbbb3c331858f11d341a72c80eae5a71fcc15}
-BOOST_TAR_NAME=boost_${BOOST_VERSION//./_}-msvc-14.2-64.exe
-BOOST_TAR_URL=${BOOST_TAR_URL:-https://archives.boost.io/release/${BOOST_VERSION}/binaries/${BOOST_TAR_NAME}}
+BOOST_TAR_URL=${BOOST_TAR_URL:-https://archives.boost.io/release/${BOOST_VERSION}/binaries/boost_${BOOST_VERSION//./_}-msvc-14.2-64.exe}
 
-echo "Boost version: $BOOST_VERSION"
-if [[ ! -e $BOOST_TAR_NAME ]]; then
-  echo "Downloading Boost binaries: '$BOOST_TAR_URL'"
-  curl -L $BOOST_TAR_URL -o $BOOST_TAR_NAME
-fi
-RESULT=$(sha256sum ${BOOST_TAR_NAME} | awk '{ print $1 }')
-if [[ $RESULT != $BOOST_TAR_HASH ]]; then
-  echo "ERROR: Boost tarball does not satisfy provided hash." >&2
-  echo " Expected: $BOOST_TAR_HASH" >&2
-  echo "   Actual: $RESULT" >&2
-  echo "Deleting this tarball." >&2
-  rm -rf $BOOST_TAR_NAME
-  exit 1
-fi
-
-TARGET_DIR="${BOOST}/prebuilds"
-echo "Preparing binaries folder: $TARGET_DIR"
-rm -rf $TARGET_DIR
-mkdir $TARGET_DIR
-tar -xzf $BOOST_TAR_NAME -C $TARGET_DIR
-content=($(ls $TARGET_DIR))
-if [[ ${#content[@]} -eq 1 ]]; then
-  mv ${TARGET_DIR}/${content[0]}/* ${TARGET_DIR}/
-  rm -rf ${TARGET_DIR}/${content[0]}
-fi
+${ROOT}/scripts/download-tar.sh Boost "${BOOST_TAR_URL}" ${BOOST_TAR_HASH} "${SCRIPT_ROOT}" "${BOOST}/prebuilds"
---
+++ thirdparty/openssl/windows/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SCRIPT_ROOT=$(realpath $(dirname $0))
+ROOT=$(realpath "${SCRIPT_ROOT}/../../..")
+OPENSSL=$(realpath "${ROOT}/thirdparty/openssl/windows")
+cd "$OPENSSL"
+
+OPENSSL_VERSION=${OPENSSL_VERSION:-3.6.2}
+OPENSSL_TAR_HASH=${OPENSSL_TAR_HASH:-c395482a1af33b2cdd4b801b227c864ed049e0f6aff79413d31b4fa916a67b1a}
+OPENSSL_TAR_URL=${OPENSSL_TAR_URL:-https://slproweb.com/download/Win64OpenSSL-${OPENSSL_VERSION//./_}.exe}
+# OPENSSL_TAR_URL=${OPENSSL_TAR_URL:-https://slproweb.com/download/Win64ARMOpenSSL-${OPENSSL_VERSION//./_}.exe}
+
+${ROOT}/scripts/download-tar.sh OpenSSL "${OPENSSL_TAR_URL}" ${OPENSSL_TAR_HASH} "${SCRIPT_ROOT}" "${OPENSSL}/prebuilds"
